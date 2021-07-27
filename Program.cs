86da5753111e5bb77111aece0e25398ee6b75b11
PlatinumMaster <l33tgamer@outlook.com>
2021-07-27 17:47:58 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Hotswap;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Write better Command Line Interface
+            if (args.Length == 2)
+            {
+                string BaseROMPath = args[0];
+                string ProjectConfigPath = args[1];
+                try
+                {
+                    new Patcher(BaseROMPath, ProjectConfigPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
